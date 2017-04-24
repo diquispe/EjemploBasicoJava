2858fd45a71bc9e28152c73804d71cfbefe9362b
diego <diego@192.168.1.35>
2017-04-24 09:32:43 -0500

    primer Commit

---
+++ Musical/src/Album.java
@@ -0,0 +1,40 @@
+
+public class Album {
+	public String titulo;
+	public String autor;
+	public float  precio;
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	//Generar Constructor para inicializar las variables
+	
+	public Album(String titulo, String autor,float precio) {
+		this.setAutor(autor);
+		this.setTitulo(titulo);
+		this.setPrecio(precio);
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public float getPrecio() {
+		return precio;
+	}
+	public void setPrecio(float precio) {
+		this.precio = precio;
+	}
+	
+	public void imprimirAlbum() {
+		System.out.println("El titulo del album es: " + this.titulo);
+		System.out.println("El composito del album es: " + this.autor);
+		System.out.println("El precio es: " + this.precio);
+	}
+	
+}
---
+++ Musical/src/PruebaAlbum.java
@@ -0,0 +1,14 @@
+
+public class PruebaAlbum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Album a = new Album("Los Grandes Exitos de Queen", "Queen", 200);
+		Album b = new Album("Los Grandes Exitos de Bach", "Bach", 30);
+		
+		a.imprimirAlbum();
+		b.imprimirAlbum();
+	}
+
+}
